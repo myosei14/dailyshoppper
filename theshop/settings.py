504f677d7d9d706f5ca39d8db7138a06d7a2e441
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2023-11-22 01:37:11 +0000

    build issue rectification

--- theshop/settings.py
+++ theshop/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'theshop/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'theshop/static')
 ]
 
 MEDIA_URL = '/images/'
