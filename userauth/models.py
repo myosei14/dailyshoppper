d5250ad023c44555ec75455a1b1dfb47df14aab0
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2023-12-08 01:17:43 +0000

    Test cases errors resolved

--- userauth/models.py
+++ userauth/models.py
@@ -8,16 +8,16 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 # custom user code begins
 # custom model manager
 class CustomUserManager(BaseUserManager):
-    def create_user(self, name, email, password, **extra_fields):
+    def create_user(self, email, name, password, **extra_fields):
         if not email:
             raise ValueError(_('Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(name=name, email=email, **extra_fields)
+        user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, name, email, password, **extra_fields):
+    def create_superuser(self, email, name, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
@@ -26,7 +26,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('Superuser must be staff'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have superuser set to true'))
-        return self.create_user(name, email, password, **extra_fields)
+        return self.create_user(email, name, password, **extra_fields)
 
 
 # user model - using AbstractBaseUser
--- userauth/tests.py
+++ userauth/tests.py
@@ -7,20 +7,20 @@ class CustomerTests(TestCase):
     def test_create_user(self):
         db = get_user_model()
         user = db.objects.create_user('test@user.com', 'name', 'password')
-        self.assertEqual(user.name, 'name')
         self.assertEqual(user.email, 'test@user.com')
-        self.assertEqual(user.password, 'password')
+        self.assertEqual(user.name, 'name')
+        self.assertTrue(user.password, user.check_password('password'))
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
 
         with self.assertRaises(ValueError):
-            db.objects.create_user(name='name', email='', password='password')
+            db.objects.create_user(email='', name='name', password='password')
 
-    def test_create_superuser(self):
+    def test_create_superuser(self): 
         db = get_user_model()
-        super_user = db.create_superuser('name', 'test@super.com', 'password')
-        self.assertEqual(super_user.name, 'name')
+        super_user = db.objects.create_superuser('test@super.com', 'name', 'password')
         self.assertEqual(super_user.email, 'test@super.com')
+        self.assertEqual(super_user.name, 'name')
         self.assertTrue(super_user.is_superuser)
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_active)
@@ -30,7 +30,7 @@ class CustomerTests(TestCase):
             db.objects.create_superuser(name='name', email='test@super.com', password='password', is_superuser=False)
 
         with self.assertRaises(ValueError):
-            db.objects.create_superuser(name='name', email='test@super.com', password='password', is_staff=False)
+            db.objects.create_superuser( name='name', email='test@super.com', password='password', is_staff=False)
 
         with self.assertRaises(ValueError):
-            db.objects.create_superuser(name='name', email='', password='password', is_superuser=True)
+            db.objects.create_superuser(name='name',email='', password='password', is_superuser=True)
